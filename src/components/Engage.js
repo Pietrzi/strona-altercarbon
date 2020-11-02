4ae6e9f882a32b0407a81c4e6c3a0c0bcf6ffc95
Pietrzi <pietrzi@op.pl>
2020-11-02 19:17:23 +0100

    basic contruction

---
+++ src/components/Engage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+//import { NavLink } from 'react-router-dom';
+
+const Engage = () => {
+    return (
+        <div className="engage-container">
+            <h2>Chcesz wiedzieć wiecej</h2>
+            <div className="buttons-wrapper">
+                <div>
+                    <h4>Masz pytanie o ofertę</h4>
+                    <NavLink to='/zapytanie'><p>Zpaytanie ofertowe</p></NavLink>
+                </div>
+                <div>
+                    <h4>Zgłoś prośbę o serwis</h4>
+                    <NavLink to='/serwis'><p>Zgłoszenie serwisowe</p></NavLink>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Engage;
